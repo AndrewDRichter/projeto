3de6e8b9891091bc7b7b692a89407723f810cca3
Andrew Richter <andrewrichter1@hotmail.com>
2025-07-03 22:03:31 -0300

    initial commit -m

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact page</h1>
+            <span>You can reach me in: +595983904571</span>
+        </div>
+    )
+}
